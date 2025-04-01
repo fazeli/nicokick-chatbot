6525badb405744342d1379e64eb94a9fc7a70ab2
Dan Fa <40899768-mohfazel@users.noreply.replit.com>
2025-04-01 12:52:23 +0000

    Convert Netlify build script to ES modules format

--- server/netlify-build.js
+++ server/netlify-build.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Make sure we're in the server directory
 try {
